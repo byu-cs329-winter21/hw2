36b2c8545636fdb71a56e8facad7ee987bbb0eb8
Nikelle Turner <graffitihooves@gmail.com>
2021-02-01 20:23:17 -0700

    finished part 1

--- src/main/java/misc/Queue.java
+++ src/main/java/misc/Queue.java
@@ -18,7 +18,13 @@ public class Queue {
   /**
    * Creates a queue with a bounded size.
    *
-   * @requires max != 0;
+   * @requires max != 0
+   *
+   * @ensures Q = fresh()
+   * @ensures size = 0 && first = 0 && last = 0
+   * @ensures Q[first] = null
+   * @ensures Q[last] = null
+   * @ensures Q.max() = max
    *
    * @param max   the size of the queue
    */
@@ -27,21 +33,48 @@ public class Queue {
     size = 0;
     first = 0;
     last = 0;
-    //this is another test
   }
 
+  /**
+   * Gets the size of the queue.
+   *
+   * @requires Q != null
+   *
+   * @ensures 0 <= size < max(Q)
+   *
+   * @return size   the number of objects in the queue
+   */
   public int size() {
     return size;
   }
 
+  /**
+   * Gets the max length of the queue.
+   *
+   * @requires Q != null
+   * @requires arr.length != 0
+   *
+   * @ensures 0 < arr.length < max(Q)
+   *
+   * @return arr.length   the max length of the queue
+   */
   public int max() {
     return arr.length;
   }
 
   /**
    * Adds a new object into the queue.
-   * 
-   * @param x the object to add to the queue
+   *
+   * @requires x != null
+   * @reguires Q != null
+   * @requires size(Q) < max(Q)
+   * @requires 0 <= last < max(Q)
+   *
+   * @ensures Q[size(old(Q))] = x
+   * @ensures size(Q) = size(old(Q)) + 1
+   * @ensures old(Q) + x = Q
+   *
+   * @param x   the object to add to the queue
    */
   public void enqueue(Object x) {
     arr[last] = x;
@@ -54,6 +87,15 @@ public class Queue {
 
   /**
    * Removes an object from the queue.
+   *
+   * @requires Q != null
+   * @requires 0 <= size(Q) < max(Q)
+   * @requires first >= 0
+   *
+   * @ensures x = Q[first]
+   * @ensures Q[first] = old(Q[first]) + 1
+   * @ensures 0 <= first < max(Q)
+   * @ensures size(Q) = size(old(Q)) - 1
    * 
    * @return the removed object
    */
